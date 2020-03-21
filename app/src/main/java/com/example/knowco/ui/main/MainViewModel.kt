69496c3b9d2f9e5c846fdd7a725fd4f158568534
Jared <j95grow@gmail.com>
2020-03-21 17:30:33 -0400

    Stop loading after non blocking delay

--- app/src/main/java/com/example/knowco/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/knowco/ui/main/MainViewModel.kt
@@ -1,8 +1,24 @@
 package com.example.knowco.ui.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    val isLoading = MutableLiveData(true)
+    private val _isLoading = MutableLiveData(true)
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        viewModelScope.launch {
+            delay(2000L)
+            _isLoading.value = false
+        }
+    }
 }
