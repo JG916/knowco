b81f2547a4f4d53c1dab994becdfb678d3410a6f
Jared <j95grow@gmail.com>
2020-03-30 20:12:33 -0400

    Attach interceptor to service

--- app/src/main/java/com/example/knowco/data/source/remote/ServiceBuilder.kt
+++ app/src/main/java/com/example/knowco/data/source/remote/ServiceBuilder.kt
@@ -2,6 +2,8 @@ package com.example.knowco.data.source.remote
 
 import com.example.knowco.data.model.Coin
 import com.google.gson.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.reflect.Type
@@ -18,8 +20,16 @@ object ServiceBuilder {
         return Retrofit.Builder()
             .baseUrl(COIN_RANK_URL)
             .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(createInterceptor())
             .build()
             .create(serviceClass)
+    }
+
+    private fun createInterceptor(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
     }
 
     private class CoinRankDeserializer<T> : JsonDeserializer<T> {
