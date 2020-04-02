2af95cdaeefc599a906def485e2cef9af96c4f32
Jared <j95grow@gmail.com>
2020-04-01 21:39:52 -0400

    Add coins request

--- app/src/main/java/com/example/knowco/data/source/remote/CoinRankService.kt
+++ app/src/main/java/com/example/knowco/data/source/remote/CoinRankService.kt
@@ -1,10 +1,14 @@
 package com.example.knowco.data.source.remote
 
 import com.example.knowco.data.model.Coin
+import com.example.knowco.data.model.CoinList
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface CoinRankService {
+    @GET("coin")
+    suspend fun getCoins(): CoinList
+
     @GET("coin/{id}")
     suspend fun getCoin(@Path(value = "id") coinId: Int): Coin
 }
